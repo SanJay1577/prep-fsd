bc3621f6cffac92a890fe2c9570327c4f30710d8
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-04-10 10:25:21 +0530

    Landing Page

--- src/Components/InterviewCrads.jsx
+++ src/Components/InterviewCrads.jsx
@@ -3,10 +3,10 @@
 /* eslint-disable react/prop-types */
 function InterviewCards({ data }) {
   return (
-    <>
+    <div className="flex flex-wrap gap-2 justify-around">
       {data &&
         data?.map((val, idx) => <InterviewCard details={val} key={idx} />)}
-    </>
+    </div>
   );
 }
 
@@ -16,14 +16,25 @@ function InterviewCard({ details }) {
   }
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <h2>{details.companyName}</h2>
-      <h3>{details.role}</h3>
-      <div>
-        <ul> {mapAllQuestions(details.Questions)}</ul>
+    <div className="card w-96 bg-accent text-black p-2 rounded-lg">
+      <h2 className="text-lg font-extrabold">{details.companyName}</h2>
+      <h3 className="text-lg font-bold">{details.role}</h3>
+
+      <div className="m-2">
+        <div className="collapse border border-base-300">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-bold">Questions</div>
+          <div className="collapse-content">
+            <ul className="pl-2 p-2"> {mapAllQuestions(details.Questions)}</ul>
+          </div>
+        </div>
       </div>
-      <p>{details.date}</p>
-      <p>{details.location}</p>
+      <p>
+        <span className="font-bold">Date : </span> {details.date}
+      </p>
+      <p>
+        <span className="font-bold">Location : </span> {details.location}
+      </p>
     </div>
   );
 }
